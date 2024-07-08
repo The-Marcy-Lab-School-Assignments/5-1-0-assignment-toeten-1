ab52ef73b7b84c40b3cab90fde73ddf20103bbcd
toeten <n.santiago0228@gmail.com>
2024-07-08 13:24:31 -0400

    did coding

--- vite-project/src/model/CartItem.js
+++ vite-project/src/model/CartItem.js
@@ -1,6 +1,11 @@
 import getId from "../utils/getId";
 
 class CartItem {
+  constructor(name, price) {
+    this.id = getId();
+    this.name = name;
+    this.price = price;
+  }
 
 }
 
--- vite-project/src/model/ShoppingCart.js
+++ vite-project/src/model/ShoppingCart.js
@@ -2,7 +2,35 @@ import getId from "../utils/getId";
 import CartItem from "./CartItem";
 
 class ShoppingCart {
+  #cartItems; 
 
+  static #allCarts = []
+
+  constructor() {
+    this.id = getId()
+    this.#cartItems = []
+    ShoppingCart.#allCarts.push(this)
+  }
+  createItem(name,price) {
+    const newItem = new CartItem(name, price);
+    this.#cartItems.push(newItem);
+    return newItem
+  }
+  getItems() {
+    return [...this.#cartItems]
+  }
+  removeItem(id) {
+    return this.#cartItems = this.#cartItems.filter(item => item.id !== id);
+  }
+  getTotal() {
+    return this.#cartItems.reduce((total, item) => total + item.price, 0);
+  }
+  static listAll() {
+    return [...ShoppingCart.#allCarts]
+  }
+  static findBy(id) {
+    return ShoppingCart.#allCarts.find(cart => cart.id === id);
+  }
 }
 
 export default ShoppingCart;
